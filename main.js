d7e00d4e7c14730cc565dfc6b64d8efd20892237
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-12-22 16:50:26 +0700

    add X button

--- main.js
+++ main.js
@@ -2,35 +2,45 @@ const toastContainer = document.createElement("div");
 toastContainer.classList.add("toast-container");
 document.body.appendChild(toastContainer);
 
-function showToast(message, duration = 3000, type = "success") {
+function showToast(message, duration = 5000, type = "success") {
   const toast = document.createElement("div");
 
   toast.classList.add("toast");
-  toast.innerHTML = message;
+  toast.innerHTML = `<div class="message-wrap">
+  <div class="message">${message}</div> <button class="closeBtn">x</button>
+  </div>`;
   toast.classList.add(type);
 
   toastContainer.appendChild(toast);
+  function closeToast() {
+    toast.style.opacity = 0;
+    setTimeout(() => {
+      toast.remove();
+    }, 1000);
+  }
+
+  const closeBtn = toast.querySelector(".closeBtn");
+  closeBtn.classList.add(`btn-${type}`);
+  console.log(closeBtn);
+  closeBtn.onclick = closeToast;
 
   setTimeout(() => {
     toast.style.opacity = 1;
-  }, 200); // 200ms sau khi bấm nút, toast sẽ hiện lên mượt mà
+  }, 200);
 
   setTimeout(() => {
-    toast.style.opacity = 0; // hành vi tạo sự biến mất của toast mượt mà nhưng ko gỡ hẳn toast đi
-    setTimeout(() => {
-      toastContainer.removeChild(toast); // hành vi gỡ hoàn toàn toast đi, đặt 3000ms để toast mượt mà biến mất
-    }, 3000);
-  }, duration); // duration là thời gian tồn tại của toast
+    closeToast();
+  }, duration);
 }
 
 const handleClick = () => {
-  showToast("Hello, this is a success toast message!", 3000, "success");
+  showToast("Hello, this is a success toast message!", 5000, "success");
 };
 
 const handleClickError = () => {
-  showToast("Hello, this is a error toast message!", 3000, "error");
+  showToast("Hello, this is a error toast message!", 5000, "error");
 };
 
 const handleClickWarning = () => {
-  showToast("Hello, this is a error toast message!", 3000, "warning");
+  showToast("Hello, this is a error toast message!", 5000, "warning");
 };
